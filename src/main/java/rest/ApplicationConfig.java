61210439e180401e52f2c730ad6ab5273e8feac0
sebastian <sebbelebbe8930@gmail.com>
2018-10-04 00:03:56 +0200

    more rest enpoints

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -19,6 +19,8 @@ public class ApplicationConfig extends Application {
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(rest.CityResource.class);
+        resources.add(rest.CompanyResource.class);
         resources.add(rest.PersonResource.class);
     }
     
