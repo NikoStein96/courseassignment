224bf03f34bd26e8a534d47d383a24b6a4ecc443
Nikolai Stein <nikolai_stein@hotmail.com>
2018-10-08 11:10:30 +0200

    fixede facade layer

--- target/generated-sources/annotations/entity/Adress_.java
+++ target/generated-sources/annotations/entity/Adress_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2018-10-08T08:59:49")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2018-10-08T11:10:14")
 @StaticMetamodel(Adress.class)
 public class Adress_ { 
 
